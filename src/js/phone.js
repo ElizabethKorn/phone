760204e40b641fde4a59d7e95c7972120bd3f8ec
Liza <lkorn036@gmail.com>
2024-09-04 16:36:05 -0500

    some changes

--- src/js/phone.js
+++ src/js/phone.js
@@ -1,9 +1,9 @@
 export default class PhoneNumberFormatter {
-    formatPhoneNumber(phone) {
-        let rightNumber = phone.replace(/[^\d+]/g, "");
-        if (rightNumber[0] === "8" && rightNumber.length === 11) {
-            rightNumber = "+7" + rightNumber.slice(1);
-        }
-        return rightNumber;
+  formatPhoneNumber(phone) {
+    let rightNumber = phone.replace(/[^\d+]/g, "");
+    if (rightNumber[0] === "8" && rightNumber.length === 11) {
+      rightNumber = "+7" + rightNumber.slice(1);
     }
+    return rightNumber;
+  }
 }
